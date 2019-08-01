693c00f3030394b2b194f9a48c3c249507c7a140
DuncanKane <47971814+DuncanKane@users.noreply.github.com>
2019-08-01 12:01:35 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/*greeting = document.getElementById('greeting'),
+focus = document.getElementById('focus');
+
+function setBgGreet ()
+{
+    greeting.textContent = 'Campaign: Battle for Damnation';
+}
+
+
+function serFocus ()
+{
+    focus.textContent = 'Coming Soon......'
+}
+
+setBgGreet();
+serFocus();*/
+
+//below is for the HTML file where this will run
+// <script src="js/main.js"></script>
